2d1c05262fc7235d74e19ae8ec4c44119c806049
RomanK <romulus_666@mail.ru>
2021-03-29 19:27:45 +0600

    DZ_8 реализована кнопка удаления продукта

--- lesson-8-dz/src/main/java/ru/romankuznetsov/controller/WebController.java
+++ lesson-8-dz/src/main/java/ru/romankuznetsov/controller/WebController.java
@@ -4,7 +4,11 @@ package ru.romankuznetsov.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.romankuznetsov.entity.Product;
 import ru.romankuznetsov.repository.ProductRepository;
 
 @Controller
@@ -15,6 +19,19 @@ public class WebController {
     @GetMapping
     public String index(Model model){
         model.addAttribute("products", repository.findAll());
+        model.addAttribute("newproduct", new Product());
         return "index";
+    }
+
+    @PostMapping
+    public String addProduct(Product product){
+        repository.save(product);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delProduct(@PathVariable long id){
+        repository.deleteById(id);
+        return "redirect:/";
     }
 }
